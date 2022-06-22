c2506a97eb4abab600e1897d13eb73a2e7d89883
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-22 20:48:32 +0530

    Add files via upload

---
+++ Single_Inheritance/Even_Odd_Child.java
@@ -0,0 +1,9 @@
+//WAP to do the process of  Even Odd Operation Using Single Inheritance
+package Inheritance;
+class Even_Odd_Child extends Even_Odd_Parent
+{
+  public static void main(String[] args)
+  {
+      even_odd();
+  }
+}
