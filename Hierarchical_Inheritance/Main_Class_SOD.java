46ff38cf7f9cd166b418a16ac7ab799919af3d9c
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-22 20:57:24 +0530

    Add files via upload

---
+++ Hierarchical_Inheritance/Main_Class_SOD.java
@@ -0,0 +1,18 @@
+package Inheritance;
+
+import java.util.Scanner;
+
+public class Main_Class_SOD {
+	static int n;
+	
+	public static int accept()
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Digit : ");
+		n = sc.nextInt();
+		System.out.println("Digit : "+n);
+		return n;
+	}
+	
+}
